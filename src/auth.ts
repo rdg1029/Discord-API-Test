8e3f9586aff091a767dce05f563f1e2ba79ea4c6
rdg1029 <wcle3456@gmail.com>
2022-10-03 17:53:46 +0900

    Update auth.ts : Apply refresh token exchange

--- src/auth.ts
+++ src/auth.ts
@@ -7,29 +7,56 @@ const CLIENT_ID = '1025232858299772999';
 const CLIENT_SECRET = 'viqjqq5DWAJuCveSGzXoa6sOkaeE05_h';
 const REDIRECT_URI = 'http://127.0.0.1:8080/auth/';
 
-const data = new URLSearchParams();
-data.append('client_id', CLIENT_ID);
-data.append('client_secret', CLIENT_SECRET);
-data.append('grant_type', 'authorization_code');
-data.append('code', CODE as string);
-data.append('redirect_uri', REDIRECT_URI);
-
-fetch(`${API_ENDPOINT}/oauth2/token`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
-    },
-    body: data,
-}).then(res => res.json())
-.then(data => {
-    console.log(data);
-    fetch(`${API_ENDPOINT}/users/@me`, {
+function exchangeToken(reqData: URLSearchParams) {
+    return fetch(`${API_ENDPOINT}/oauth2/token`, {
+        method: 'POST',
         headers: {
-            authorization: `${data.token_type} ${data.access_token}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: reqData,
+    });
+}
+
+function getUserInfo(accessToken: string) {
+    return fetch(`${API_ENDPOINT}/users/@me`, {
+        headers: {
+            authorization: `Bearer ${accessToken}`,
         }
-    }).then(res => res.json())
-    .then(data => console.log(data))
+    });
+}
+
+if (sessionStorage.getItem('refresh_token')) {
+    const data = new URLSearchParams();
+    data.append('client_id', CLIENT_ID);
+    data.append('client_secret', CLIENT_SECRET);
+    data.append('grant_type', 'refresh_token');
+    data.append('refresh_token', sessionStorage.getItem('refresh_token') as string);
+
+    exchangeToken(data).then(res => res.json())
+    .then(data => {
+        console.log(data);
+        sessionStorage.setItem('refresh_token', data.refresh_token);
+        getUserInfo(data.access_token).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(e => console.log(e));
+    })
     .catch(e => console.log(e));
-})
-.catch(e => console.log(e));
+}
+else {
+    const data = new URLSearchParams();
+    data.append('client_id', CLIENT_ID);
+    data.append('client_secret', CLIENT_SECRET);
+    data.append('grant_type', 'authorization_code');
+    data.append('code', CODE as string);
+    data.append('redirect_uri', REDIRECT_URI);
+
+    exchangeToken(data).then(res => res.json())
+    .then(data => {
+        console.log(data);
+        sessionStorage.setItem('refresh_token', data.refresh_token);
+        getUserInfo(data.access_token).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(e => console.log(e));
+    })
+    .catch(e => console.log(e));
+}
