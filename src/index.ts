3d7e58cad5b3069e0739502821e1aa4af8f424ba
rdg1029 <wcle3456@gmail.com>
2022-10-04 17:16:38 +0900

    Update index.ts : Add requestApi()

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,8 @@ window.onload = () => {
     const loginLink = document.getElementById('login-with-discord') as HTMLLinkElement;
     const contents = document.getElementById('contents') as HTMLDivElement
     const userName = document.getElementById('user-name') as HTMLParagraphElement;
-    
-    if (sessionStorage.getItem('refresh_token')) {
-        const data = new URLSearchParams();
-        data.append('client_id', API.CLIENT_ID);
-        data.append('client_secret', API.CLIENT_SECRET);
-        data.append('grant_type', 'refresh_token');
-        data.append('refresh_token', sessionStorage.getItem('refresh_token') as string);
-    
+
+    function requestApi(data: URLSearchParams) {
         API.exchangeToken(data).then(res => res.json())
         .then(data => {
             console.log(data);
@@ -35,6 +29,16 @@ window.onload = () => {
         })
         .catch(e => console.log(e));
     }
+    
+    if (sessionStorage.getItem('refresh_token')) {
+        const data = new URLSearchParams();
+        data.append('client_id', API.CLIENT_ID);
+        data.append('client_secret', API.CLIENT_SECRET);
+        data.append('grant_type', 'refresh_token');
+        data.append('refresh_token', sessionStorage.getItem('refresh_token') as string);
+    
+        requestApi(data);
+    }
     else {
         if (!CODE) return;
         const data = new URLSearchParams();
@@ -44,23 +48,6 @@ window.onload = () => {
         data.append('code', CODE as string);
         data.append('redirect_uri', API.REDIRECT_URI);
     
-        API.exchangeToken(data).then(res => res.json())
-        .then(data => {
-            console.log(data);
-            sessionStorage.setItem('refresh_token', data.refresh_token);
-    
-            API.getUserInfo(data.access_token).then(res => res.json())
-            .then(data => {
-                loginLink.style.display = 'none';
-                userName.innerText = data.username;
-                contents.style.display = "unset";
-            })
-            .catch(e => console.log(e));
-    
-            API.getUserGuilds(data.access_token).then(res => res.json())
-            .then(data => console.log(data))
-            .catch(e => console.log(e));
-        })
-        .catch(e => console.log(e));
+        requestApi(data);
     }
 }
